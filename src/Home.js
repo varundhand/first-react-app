ef082c48e4108a73fffd49ec72c3391548981577
varundhand <varundhand77@gmail.com>
2023-04-07 18:27:13 +0530

    click events with arguments

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,22 @@
 const Home = () => {
+  // const handleClick = () => { // we dont envoke this function below because it runs itself without even clicking the btn
+  //   console.log("button is clicked");
+  // };
+  const handleClick = (event) => {
+    console.log("hello ", "event is: ", event);
+  };
+  const handleClickAgain = (name, event) => {
+    console.log("hello", name, event);
+  };
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <button
+        onClick={(event) => handleClickAgain("varun dhand", event.target)}
+      >
+        Click me Again
+      </button>
     </div>
   );
 };
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,9 @@
 const Navbar = () => {
+  // const linkStyling = {
+  //   color: "white",
+  //   backgroundColor: "#f1356d",
+  //   borderRadius: "8px",
+  // };
   return (
     <nav className="navbar">
       <h1>The Blog App</h1>
