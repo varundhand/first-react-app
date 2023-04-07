9c43a67c6027043169d58b443708e45b16287e92
varundhand <varundhand77@gmail.com>
2023-04-07 16:17:19 +0530

    added styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import "./App.css";
 import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
   // const person = { name: "varun", age: 19 }; // react converts the data types into string
   return (
     <div className="App">
       <Navbar />
-      <div className="content"></div>
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
