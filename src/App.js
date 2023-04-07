0bb4c039b953d3c8a0f3dff6afd3dbd15b2f13bc
varundhand <varundhand77@gmail.com>
2023-04-07 15:36:10 +0530

    learning react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./Navbar";
 
 function App() {
+  // const person = { name: "varun", age: 19 }; // react converts the data types into string
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="content"></div>
     </div>
   );
 }
 
-export default App;
+export default App; // can write export default before the 'function App()' also
