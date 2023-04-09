bbcde47a94ad3a591be587b3cbb8c0009410fe80
varundhand <varundhand77@gmail.com>
2023-04-10 02:00:29 +0530

    react-router-dom added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   // const person = { name: "varun", age: 19 }; // react converts the data types into string
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
